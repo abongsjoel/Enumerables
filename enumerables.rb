f1184a5fa6cf1c83b36019574d2236394058595b
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2020-12-03 11:14:49 -0600

    implement select method into program

--- enumerables.rb
+++ enumerables.rb
@@ -16,6 +16,16 @@ module Enumerable
     end
     self
   end
+
+  def my_select
+    return to_enum(:select) unless block_given?
+    
+    arr = Array.new
+    0.upto(to_a.length - 1) do |i|
+      arr << to_a[i] if yield(to_a[i])
+    end
+    arr
+  end
 end
 
 names = ["Jane", "John", "Philip", "Emmmanuel"]
@@ -24,3 +34,5 @@ puts "------my_each-----"
 names.my_each { |name| puts "Hello #{name}"}
 puts "------my_each_with_index-------"
 names.my_each_with_index { |name, index| puts "#{name}'s index is #{index}"}
+puts "------my_select-------"
+p names.my_select { |name| name == "Jane"}
