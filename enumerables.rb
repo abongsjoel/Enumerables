928d38e2bb65f82d914fb28ceb59c1219343f0ae
tj-xubuntu <abongsjoel@gmail.com>
2020-12-03 17:53:44 +0100

    Implement my_each_with_index

--- enumerables.rb
+++ enumerables.rb
@@ -2,9 +2,18 @@ module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
     
-  0.upto(length - 1) do |i|
-    yield([i])
+    0.upto(to_a.length - 1) do |i|
+      yield(to_a[i])
+    end
+    self
   end
+
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+    
+    0.upto(to_a.length - 1) do |i|
+      yield(to_a[i], i)
+    end
     self
   end
 end
@@ -13,3 +22,5 @@ names = ["Jane", "John", "Philip", "Emmmanuel"]
 
 puts "------my_each-----"
 names.my_each { |name| puts "Hello #{name}"}
+puts "------my_each_with_index-------"
+names.my_each_with_index { |name, index| puts "#{name}'s index is #{index}"}
