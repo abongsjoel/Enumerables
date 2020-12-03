5d5d7e29549d3f587bfd18110a81eefa8fb9f714
tj-xubuntu <abongsjoel@gmail.com>
2020-12-03 17:48:59 +0100

    Implement my_each

--- enumerables.rb
+++ enumerables.rb
@@ -1,5 +1,15 @@
 module Enumerable
-    def my_each
-      # your code here
-    end
+  def my_each
+    return to_enum(:my_each) unless block_given?
+    
+  0.upto(to_a.length - 1) do |i|
+    yield(to_a[i])
   end
+    self
+  end
+end
+
+names = ["Jane", "John", "Philip", "Emmmanuel"]
+
+puts "------my_each-----"
+names.my_each { |name| puts "Hello #{name}"}
