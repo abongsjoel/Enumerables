0ec04322c53ed686ca003a072440d205f16de50e
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2020-12-04 11:01:14 -0600

    fix linter errors

--- enumerables.rb
+++ enumerables.rb
@@ -1,7 +1,9 @@
+# rubocop: disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
+
 module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
-    
+
     0.upto(to_a.length - 1) do |i|
       yield(to_a[i])
     end
@@ -10,7 +12,7 @@ module Enumerable
 
   def my_each_with_index
     return to_enum(:my_each_with_index) unless block_given?
-    
+
     0.upto(to_a.length - 1) do |i|
       yield(to_a[i], i)
     end
@@ -19,40 +21,40 @@ module Enumerable
 
   def my_select
     return to_enum(:my_select) unless block_given?
-    
-    arr = []
-    to_a.my_each { |val| arr << val if yield(val)}
 
-    (is_a? Array)? arr : arr.to_h
+    arr = []
+    to_a.my_each { |val| arr << val if yield(val) }
+
+    is_a?(Array) ? arr : arr.to_h
   end
 
   def my_all?
     return true unless block_given?
 
-    to_a.my_each { |val| return false unless yield(val)}
+    to_a.my_each { |val| return false unless yield(val) }
     true
   end
 
   def my_any?
     return true unless block_given?
 
-    to_a.my_each { |val| return true if yield(val)}
+    to_a.my_each { |val| return true if yield(val) }
     false
   end
 
   def my_none?
     return true unless block_given?
 
-    to_a.my_each { |val| return false if yield(val)}
+    to_a.my_each { |val| return false if yield(val) }
     true
   end
 
-  def my_count?(para = false)
+  def my_count?(para = nil)
     counter = 0
     if block_given?
-      to_a.my_each { |val| counter+=1 if yield(val) }
+      to_a.my_each { |val| counter += 1 if yield(val) }
     elsif para
-      to_a.my_each { |val| counter+=1 if para == val }  
+      to_a.my_each { |val| counter += 1 if para == val }
     else
       counter = to_a.length
     end
@@ -60,7 +62,7 @@ module Enumerable
   end
 
   def my_map(my_proc = nil)
-    return to_enum(:my_map) unless block_given? || my_proc 
+    return to_enum(:my_map) unless block_given? || my_proc
 
     arr = []
     if my_proc
@@ -71,76 +73,28 @@ module Enumerable
     arr
   end
 
-  def my_inject(initial_1 = false, initial_2 = false)
-    if initial_1.is_a?(Symbol) && initial_2 == false
+  def my_inject(initial_1 = nil, initial_2 = nil)
+    if initial_1.is_a?(Symbol) && !initial_2
       memo = to_a[0]
-      1.upto(to_a.length - 1 ) { |i| memo = memo.send(initial_1, to_a[i])} 
+      1.upto(to_a.length - 1) { |i| memo = memo.send(initial_1, to_a[i]) }
     elsif initial_1.is_a?(Integer) && initial_2.is_a?(Symbol)
       memo = initial_1
-      0.upto(to_a.length - 1 ) { |i| memo = memo.send(initial_2, to_a[i])} 
+      0.upto(to_a.length - 1) { |i| memo = memo.send(initial_2, to_a[i]) }
+    elsif block_given? && initial_1.is_a?(Integer)
+      memo = initial_1
+      to_a.my_each { |val| memo = yield(memo, val) }
+    elsif block_given? && !initial_1
+      memo = to_a[0]
+      1.upto(to_a.length - 1) { |i| memo = yield(memo, to_a[i]) }
     else
-      if block_given? && initial_1
-        memo = initial_1
-        to_a.my_each { |val| memo = yield(memo, val)}
-      elsif block_given? && !initial_1
-        memo = to_a[0]
-        1.upto(to_a.length - 1 ) { |i| memo = yield(memo, to_a[i])}
-      else
-        return "input error"
-      end
+      return 'input error'
     end
     memo
   end
 end
 
+# rubocop: enable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
+
 def multiply_els(arr)
   arr.my_inject(:*)
 end
-
-
-
-names = ["Jane", "John", "Philip", "Emmmanuel", "John"]
-hash_names = {:Jane => "1", :John => "2", "Philip" => "3", "Emmmanuel" => "4"}
-numbers = [3, 4, 5, 1]
-
-puts "------#my_each-----"
-names.my_each { |name| puts "Hello #{name}"}
-hash_names.my_each { |k,v| puts "#{k} is #{v}"}
-puts "------#my_each_with_index-------"
-names.my_each_with_index { |name, index| puts "#{name}'s index is #{index}"}
-puts "------#my_select-------"
-p names.my_select { |name| name == "Jane"}
-p hash_names.my_select { |k, v| v === "2"}
-puts "--------#my_all-------"
-p names.my_all? { |name| name.length > 4 }
-p hash_names.my_all? { |k, v| v.is_a? String }
-puts "--------#my_any-------"
-p names.my_any? { |name| name.length < 5 }
-p hash_names.my_any? { |k, v| v.is_a? Array }
-puts "--------#my_none-------"
-p names.my_none? { |name| name.length < 1 }
-p hash_names.my_none? { |k, v| v.is_a? String }
-puts "--------#my_count------"
-p names.my_count? { |name| name.length == 4}
-p names.my_count?("John")
-p names.my_count?
-puts "---------#my_map------------"
-upcase_proc = Proc.new { |name| name.upcase}
-p names.my_map(upcase_proc) 
-p numbers.my_map { |num| num ** 2 }
-puts "---------#my_inject----------"
-p numbers.my_inject { |sum, n| sum + n }
-p numbers.my_inject(100) { |sum, n| sum + n }
-longest = %w{ cat sheep bear }.inject do |memo, word|
-  memo.length > word.length ? memo : word
-end
-p longest   
-p numbers.my_inject(:+)
-p numbers.my_inject(:*)
-p numbers.my_inject(2, :+)
-#incorrect arguments passed
-p numbers.my_inject(:+, 2) #=> "input error"
-# argument and block passed
-p (5..10).my_inject(1) { |product, n| product * n } #=> 151200
-puts "---------#multiply_els----------"
-p multiply_els([2,4,5]) #=> 40
